e922923120cbefe17c8dd5c30e4557db34909416
Brazier85 <github@berger-em.de>
2024-10-14 20:16:07 +0200

    feat(): Add run script for servers

---
+++ run_server.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Description: This script controls the execution of a Python script with a start and stop parameter.
+# Usage: ./script.sh start|stop
+# "start" will run the Python script in the background and save its process ID.
+# "stop" will terminate the process using the saved process ID.
+
+# Define the path to the Python script
+SCRIPT="./main.py"
+LOG_FILE="Ficit2Discord.log"
+PID_FILE="current.pid"
+
+# Check the input parameter
+case "$1" in
+start)
+    if [ -f "$PID_FILE" ]; then
+        echo "Script is already running with PID: $(cat $PID_FILE)"
+        exit 1
+    fi
+    echo "Starting script..."
+    nohup $SCRIPT >$LOG_FILE 2>&1 &
+    echo $! >$PID_FILE
+    echo "Script started with PID: $(cat $PID_FILE)"
+    ;;
+
+stop)
+    if [ ! -f "$PID_FILE" ]; then
+        echo "No script is running."
+        exit 1
+    fi
+    PID=$(cat $PID_FILE)
+    echo "Stopping script with PID: $PID"
+    kill -9 $PID
+    rm -f $PID_FILE
+    echo "Script stopped."
+    ;;
+
+*)
+    echo "Usage: $0 start|stop"
+    exit 1
+    ;;
+esac
