1bc2c6c4e10b00268fb3fb50020bdc206335c119
Brazier85 <1213924+Brazier85@users.noreply.github.com>
2024-10-16 16:49:40 +0200

    Added access roles (#1)

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ load_dotenv()
 DC_TOKEN = os.getenv("DISCORD_TOKEN", "")
 DC_GUILD = os.getenv("DISCORD_GUILD")
 DC_OWNER = os.getenv("DISCORD_BOT_OWNER", "")
+DC_SF_ADMIN_ROLE = os.getenv("DISCORD_SF_ADMIN_ROLE", "Ficsit2Discord")
 SF_IP = os.getenv("SF_IP", "127.0.0.1")
 SF_PORT = os.getenv("SF_PORT", "7777")
 SF_TOKEN = os.getenv("SF_TOKEN", "")
@@ -94,25 +95,6 @@ async def on_command_error(ctx, error):
         )
         print(f"Ignoring exception in command {ctx} | !")
 
-
-@bot.command(name="create_role")
-@commands.is_owner()
-async def create_role(ctx):
-    role_name = "Ficsit2Discord"
-    guild = ctx.guild
-    if role_name in ctx.guild.roles["name"]:
-        await ctx.send(f"Role `{role_name}` already exists.")
-    else:
-        try:
-            await guild.create_role(name=role_name, hoist=True)
-        except Exception as e:
-            print(e)
-            await ctx.send(f"Could not create role `{role_name}`.")
-        else:
-            print(f"Role {role_name} created")
-            await ctx.send(f"I created role `{role_name}`.")
-
-
 # Loading cogs into the bot
 # see initial_extensions variable
 async def load_cogs():
@@ -130,6 +112,7 @@ def main():
     # Connect to the Satisfactory server
     bot.api = API(address=f"{SF_IP}:{SF_PORT}", token=SF_TOKEN)
     bot.server = SF_SERVER_NAME
+    bot.dc_sf_admin_role = DC_SF_ADMIN_ROLE
     # Login into Discord
     bot.run(DC_TOKEN, reconnect=True)
 
